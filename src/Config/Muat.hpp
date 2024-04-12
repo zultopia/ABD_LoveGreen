32a026d30a8620016880557332579ff013c217f6
FedrianzD <fedrianzd@gmail.com>
2024-04-13 01:31:21 +0700

    fix muat

--- src/Config/Muat.hpp
+++ src/Config/Muat.hpp
@@ -27,7 +27,10 @@ class Muat: virtual SimpanMuat{
         static void muat();
         static void read(ifstream& inputFile);
         static void setUp();
-        
+        static void setUpPetani(Pemain* player, string nama);
+        static void setUpPeternak(Pemain* player, string nama);
+        static void setUpWalikota(Pemain* player, string nama);
+        static void setUpInventory(Pemain* pemain);
 };
 
 #endif
